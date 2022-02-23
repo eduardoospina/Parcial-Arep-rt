84b0f1fd87a4c363a56dc8b27f3de526f298c8ca
unknown <eduardo.ospina@labinfo.is.escuelaing.edu.co>
2022-02-23 18:10:59 -0500

    consultas

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -63,13 +63,14 @@ public class HttpServer {
                         + "</head>"
                         + "<body>"
                         + "<h1>  Ciudad a buscar</h1>"
+                        + "<form"
                         + "<input type=\"text\" class=\"form-control\" placeholder=\"Clima\" id=\"Clima\" style=\"margin-bottom: 10px\">"
                         + "<input id=\"button-Clima\" type=\"submit\" value=\"Buscar\" class=\"btns\" >"
                         + "<h4  id=\"climasl\"></h4>"
                         + "</body>"
                         + "</html>" + inputLine;
             }
-            else if (file.startsWith("/Consultas?lugar=")){
+            else if (file.contains("/Consultas?lugar=")){
                 outputLine = "HTTP/1.1 200 OK\r\n"
                         + "Conten-Type: text/html\r\n"
                         + "\r'n";
